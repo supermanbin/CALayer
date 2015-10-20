505e481b8989eb33a2eb9367b67afcf53d665ad4
fengbinsuperman <fengbinsuperman@gmail.com>
2015-10-20 21:18:15 +0800

    changes log

---
+++ CALayer/CALayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CALayer
+//
+//  Created by 封斌 on 15/10/20.
+//  Copyright © 2015年 supermanUI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
